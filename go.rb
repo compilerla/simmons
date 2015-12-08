96bfe37ad48760ddefc043692e2b4b1bdf9f83b0
Stephen Corwin <corwinstephen@gmail.com>
2015-12-07 17:33:45 -0800

    Start learning some stuff about our files

--- go.rb
+++ go.rb
@@ -1,3 +1,21 @@
+require 'roo'
 require './property.rb'
 
-puts "I don't do anything yet"
+easy = []
+annoying = []
+
+Dir.foreach('./files') do |item|
+  next if item == '.' or item == '..'
+  
+  begin
+    sheet = Roo::Spreadsheet.open("./files/#{item}")
+    if (sheet.row(1).map(&:downcase) & ['apn', 'ain', 'address']).any?
+      easy << item
+    end
+  rescue
+    annoying << item
+  end
+
+end
+
+puts "#{easy.count} easy files. #{annoying.count} annoying ones."
