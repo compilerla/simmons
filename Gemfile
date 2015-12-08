b85a113ed8b6fc3b1185f36f53642d2451a45998
Stephen Corwin <corwinstephen@gmail.com>
2015-12-08 13:27:01 -0800

    one down

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.2.0'
 source 'https://rubygems.org'
 
 gem 'roo'
+gem 'geocoder'
---
+++ data_processors/leased_properties.rb
@@ -0,0 +1,29 @@
+require 'geocoder'
+
+file_name = "Leased properties to NPOs - GSD - 110 - FY 2014.csv"
+
+output = []
+errors = []
+CSV.foreach("../data/raw/#{file_name}") do |row|
+  begin
+    addy = row[2]
+    puts "Geocoding #{addy}"
+    geo_response = Geocoder.search(addy).first
+    compiled_addy = geo_response.address
+    lat = geo_response.coordinates[0]
+    lng = geo_response.coordinates[1]
+    row[14] = compiled_addy
+    row[15] = lat
+    row[16] = lng
+
+    output << row
+  rescue
+    errors << { row: row, addy: addy }
+  end
+end
+
+CSV.open("../data/compiled/#{file_name}", "wb") do |csv|
+  output.each do |row|
+    csv << row
+  end
+end
