1c742f68857739a128ff14a290cba1e77b5d2e18
Stephen Corwin <corwinstephen@gmail.com>
2015-12-10 17:42:37 -0800

    Start cleaning APNs

--- data_processors/mergeall.rb
+++ data_processors/mergeall.rb
@@ -1,316 +1,73 @@
 require 'csv'
 
-file_name = "2015 Registered Foreclosed Properties.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "wb")
-
-out_csv << ['source', 'apn_given', 'address_given', 'address']
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['Property Address'], '']
+class Housekeeping
+  def self.clean_apn(apn)
+    return [''] if apn.nil?
+    if apn =~ /^.{4}-.{3}-.{3}$/
+      [apn.gsub(/^(.{4})-(.{3})-(.{3})$/, '\1\2\3')]
+    elsif apn.include?(';')
+      apn.split(';').map(&:strip)
+    elsif apn.include?(',')
+      apn.split(',').map do |no_commas_apn|
+        clean_apn(no_commas_apn)
+      end
+    else
+      [apn]
+    end
+  end
 end
 
-in_csv.close
-out_csv.close
+class Merger
+  def self.merge_file(file_name, apn_col, address_col, new_file = false)
+    open_mode = new_file ? 'wb' : 'ab'
+    in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
+    out_csv = CSV.open("../data/master_with_dups.csv", open_mode)
+    in_csv.each do |row|
+      apn_raw = apn_col.nil? ? '' : row[apn_col]
+      
+      apns_cleaned = Housekeeping.clean_apn(apn_raw)
+      address = address_col.nil? ? '' : build_column_value(row, address_col)
 
+      apns_cleaned.each do |clean_apn|
+        out_csv << [file_name, clean_apn, address, '']
+      end
+    end
 
+    in_csv.close
+    out_csv.close
+  end
 
-file_name = "Assumed outside LA City Limits.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['AIN'], row['PropertyLocation'], '']
+  def self.build_column_value(input_row, keys)
+    return '' if keys.nil? || keys == ''
+    return input_row[keys] unless keys.kind_of?(Array)
+    keys.map do |key|
+      input_row[key]
+    end.join(' ')
+  end
 end
 
-in_csv.close
-out_csv.close
-
-
-file_name = "Brownfields Program - Sanitation Department.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Building Book - GSD - 4468 FY 2014_by_building_book_number.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], "#{row['Street #']} #{row['Street Dir']} #{row['Street Name']} #{row['Street Type']} #{row['Zip Code']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Building Book - GSD - 4468 FY 2014_listed_by_address.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], "#{row['Street #']} #{row['Street Dir']} #{row['Street Name']} #{row['Street Type']} #{row['Zip Code']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "City Owned Within CDs.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['AIN'], '', '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "CRA Option Properties .csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "CRA Property List Oct 2012.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['Parcel Number'], row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Decommissioned Fire Stations.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], '', '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "GSD Facilities  For Filming.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Insured Buildings & Uninsured Buildings -CAO - 359.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['Address']}, #{row['City']}, #{row['State']}, #{row['Zip']} #{row['Zip Code']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Leased properties to NPOs - GSD - 110 - FY 2014.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Master Property List (Simple) 03-7-2013- Update (1) (2).csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "MICLA Commercial Paper Note Program.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', row['ADDRESS'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Neighborhood Land Trust Empty Lots .csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['AIN'], '', '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Own a Piece of LA - OPLA (3).csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['ADDRESS'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Parking Lots & Structures - LADOT.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['Address']}, #{row['Zip Code']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Projected Surplus Properties Sales FY15-16 (Per GSD).csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], "#{row['PROPERTY ADDRESS']}, #{row['ZIP']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Properties in BIDS - Compiled.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['Site Addr'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-file_name = "Properties Recommended for Disposition byb HCIDLA.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['Property Address']}, #{row['City']}, #{row['Zip Code']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Recreation and Parks - Provided by Department.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row["APN"], row['Address'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Reported Nuisance Properties FYs 14-16__14-15_sheet.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['Street #']} #{row['Street Name']}, #{row['City & ZIP']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Reported Nuisance Properties FYs 14-16__15-16_sheet.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['Street #']} #{row['Street Name']}, #{row['City & ZIP']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "Residential Leases - GSD - 11 total - FY 2013.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, '', "#{row['ADDRESS']}, #{row['ADDRESS_2']}", '']
-end
-
-in_csv.close
-out_csv.close
-
-
-
-file_name = "undeclared surplus property by id.csv"
-
-in_csv = CSV.open("../data/raw/#{file_name}", { headers: true })
-out_csv = CSV.open("../data/master_with_dups.csv", "ab")
-in_csv.each do |row|
-  out_csv << [file_name, row['APN'], row['ADDRESS'], '']
-end
-
-in_csv.close
-out_csv.close
-
-
+# Merger.merge_file("2015 Registered Foreclosed Properties.csv", 'APN', 'Property Address', true)
+# Merger.merge_file("Assumed outside LA City Limits.csv", 'AIN', 'PropertyLocation')
+# Merger.merge_file("Brownfields Program - Sanitation Department.csv", 'APN', 'Address')
+Merger.merge_file("Building Book - GSD - 4468 FY 2014_by_building_book_number.csv", 'APN', ['Street #', 'Street Dir', 'Street Name', 'Street Type', 'Zip Code'], true)
+Merger.merge_file("Building Book - GSD - 4468 FY 2014_listed_by_address.csv", 'APN', ['Street #', 'Street Dir', 'Street Name', 'Street Type', 'Zip Code'])
+# Merger.merge_file("City Owned Within CDs.csv", 'AIN', nil)
+# Merger.merge_file("CRA Option Properties .csv", nil, 'Address')
+# Merger.merge_file("CRA Property List Oct 2012.csv", 'Parcel Number', 'Address')
+# Merger.merge_file("Decommissioned Fire Stations.csv", 'APN', nil)
+# Merger.merge_file("GSD Facilities  For Filming.csv", nil, 'Address')
+# Merger.merge_file("Insured Buildings & Uninsured Buildings -CAO - 359.csv", nil, ['Address','City', 'State', 'Zip'])
+# Merger.merge_file("Leased properties to NPOs - GSD - 110 - FY 2014.csv", nil, 'Address')
+# Merger.merge_file("Master Property List (Simple) 03-7-2013- Update (1) (2).csv", 'APN', 'Address')
+# Merger.merge_file("MICLA Commercial Paper Note Program.csv", nil, 'ADDRESS')
+# Merger.merge_file("Neighborhood Land Trust Empty Lots .csv", 'AIN', nil)
+# Merger.merge_file("Own a Piece of LA - OPLA (3).csv", 'APN', 'ADDRESS')
+# Merger.merge_file("Parking Lots & Structures - LADOT.csv", nil, ['Address', 'Zip Code'])
+# Merger.merge_file("Projected Surplus Properties Sales FY15-16 (Per GSD).csv", 'APN', ['PROPERTY ADDRESS', 'ZIP'])
+# Merger.merge_file("Properties in BIDS - Compiled.csv", 'APN', 'Site Addr')
+# Merger.merge_file("Properties Recommended for Disposition byb HCIDLA.csv", nil, ['City', 'Zip Code'])
+# Merger.merge_file("Recreation and Parks - Provided by Department.csv", 'APN', 'Address')
+# Merger.merge_file("Reported Nuisance Properties FYs 14-16__14-15_sheet.csv", nil, ['Street #', 'Street Name', 'City & ZIP'])
+# Merger.merge_file("Reported Nuisance Properties FYs 14-16__15-16_sheet.csv", nil, ['Street #', 'Street Name', 'City & ZIP'])
+# Merger.merge_file("Residential Leases - GSD - 11 total - FY 2013.csv", nil, ['ADDRESS', 'ADDRESS_2'])
+# Merger.merge_file("undeclared surplus property by id.csv", 'APN', 'ADDRESS')
