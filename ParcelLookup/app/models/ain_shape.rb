1dea806a1cb866bd32a39b31161ab6981c035b4f
Scott Hoch <scotthoch86@gmail.com>
2015-12-14 20:14:22 -0800

    seed task to populate database with shapes

--- ParcelLookup/app/models/ain_shape.rb
+++ ParcelLookup/app/models/ain_shape.rb
@@ -1,3 +1,9 @@
 class AinShape < ActiveRecord::Base
   scope :containing_point, -> (point) { where("ain_shapes.shape && ?", point) }
+
+  def self.find_by_lat_lon(lat, lon)
+    factory = RGeo::Geographic.simple_mercator_factory()
+    point = factory.point(lon, lat)
+    self.containing_point(point)
+  end
 end
--- ParcelLookup/db/seeds.rb
+++ ParcelLookup/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+require 'json'
+
+factory = RGeo::Geographic.simple_mercator_factory()
+
+file = File.read('../data/geo.geojson');0
+data_hash = JSON.parse(file);0
+
+data_hash['features'].each do |parcel|
+  next unless parcel['geometry']['type'] == "Polygon"
+  p parcel["geometry"]["coordinates"].first
+  parcel_points = parcel["geometry"]["coordinates"].first.map do |coords|
+    factory.point(coords[0], coords[1])
+  end
+  parcel_string = factory.line_string(parcel_points)
+  parcel_polygon = factory.polygon(parcel_string)
+
+  AinShape.create(ain: parcel['properties']['id'], shape: parcel_polygon)
+end
